f9dfffb63432c319f8618401fd3d5e64a4cf0c31
Björn Dieding <bjoern@xrow.de>
2007-11-16 18:39:16 +0000

    corrected german translation
    git-svn-id: http://svn.projects.ez.no/birthday@4 fe55e3c2-9ced-44be-a791-3098d20d65d4

--- trunk/extension/birthday/translations/ger-DE/translation.ts
+++ trunk/extension/birthday/translations/ger-DE/translation.ts
@@ -3,65 +3,65 @@
     <name>design/standard/class/datatype</name>
     <message>
         <source>Default value</source>
-        <translation>Standard Verdi</translation>
+        <translation>Standard Wert</translation>
     </message>
     <message>
         <source>Empty</source>
-        <translation>Tom</translation>
+        <translation>Leer</translation>
     </message>
     <message>
         <source>Current date</source>
-        <translation>Dato Nå</translation>
+        <translation>Aktuelles Datum</translation>
     </message>
 </context>
 <context>
     <name>design/standard/content/datatype</name>
     <message>
         <source>Day</source>
-        <translation>Dag</translation>
+        <translation>Tag</translation>
     </message>
     <message>
         <source>Month</source>
-        <translation>Måned</translation>
+        <translation>Monat</translation>
     </message>
     <message>
         <source>Year</source>
-        <translation>År</translation>
+        <translation>Jahr</translation>
     </message>
 </context>
 <context>
     <name>kernel/classes/datatypes</name>
     <message>
         <source>Missing date input.</source>
-        <translation>Mangler dato input.</translation>
+        <translation>Fehlendes Datum.</translation>
     </message>
     <message>
         <source>Aries</source>
-        <translation>Væren</translation>
+        <translation>Widder</translation>
     </message>
     <message>
         <source>Taurus</source>
-        <translation>Tyren</translation>
+        <translation>Stier</translation>
     </message>
     <message>
         <source>Gemini</source>
-        <translation>Tvillingene</translation>
+        <translation>Zwilling</translation>
     </message>
     <message>
         <source>Cancer</source>
-        <translation>Kreps</translation>
+        <translation>Krebs</translation>
     </message>
     <message>
         <source>Leo</source>
-        <translation>Løve</translation>
+        <translation>Löwe</translation>
     </message>
     <message>
         <source>Virgio</source>
-        <translation>Jomfru</translation>
+        <translation>Jungfrau</translation>
     </message>
     <message>
         <source>Libra</source>
-        <translation>Vekten</translation>
+        <translation>Waage</translation>
     </message>
     <message>
         <source>Scorpio</source>
@@ -69,24 +69,24 @@
     </message>
     <message>
         <source>Sagittarius</source>
-        <translation>Skytten</translation>
+        <translation>Schütze</translation>
     </message>
     <message>
         <source>Capricorn</source>
-        <translation>Steinbukk</translation>
+        <translation>Steinbock</translation>
     </message>
     <message>
         <source>Aquarius</source>
-        <translation>Vannmann</translation>
+        <translation>Wassermann</translation>
     </message>
     <message>
         <source>Pisces</source>
-        <translation>Fisk</translation>
+        <translation>Fische</translation>
     </message>
     <message>
         <source>Birthday</source>
         <comment>Datatype name</comment>
-        <translation>Bursdag</translation>
+        <translation>Geburtstag</translation>
     </message>
 </context>
 </TS>
